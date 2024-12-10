ffdce966401cbb0bd5758f366c5fe0dd770f07d6
leon5678hk <pllo1@myseneca.ca>
2024-12-10 11:18:53 -0500

    Update tsla_revenue_data.py

--- tsla_revenue_data.py
+++ tsla_revenue_data.py
@@ -11,13 +11,13 @@ soup = BeautifulSoup(html_data, "html.parser")
 
 table = soup.find("table", class_="table")
 
-tsla_data = []
+tsla_revenue = []
 for row in table.find("tbody").find_all("tr"):
     columns = row.find_all("td")
     year = columns[0].get_text(strip=True)
     revenue = columns[1].get_text(strip=True)
     change = columns[2].get_text(strip=True)
-    tsla_data.append({"Year": year, "Revenue": revenue, "Change": change})
+    tsla_revenue.append({"Year": year, "Revenue": revenue, "Change": change})
 
 tsla_df = pd.DataFrame(revenue_data)
 
